284d76123966a27128eeb0901eb1b51c44c0e5c8
bendecre <bed1@umbc.edu>
2022-02-10 04:38:03 -0500

    Initial commit

---
+++ mongodb.js
@@ -0,0 +1,15 @@
+// CRUD create read update delete
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database')
+    }
+
+    
+})
