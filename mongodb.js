48471e61dff8779ba48d4a6b3e7686e6d2e8eb00
bendecre <bed1@umbc.edu>
2022-02-10 16:20:56 -0500

    Changed functions in mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -5,10 +5,6 @@ const { MongoClient, ObjectId } = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectId()
-console.log(id.id.length)
-console.log(id.toHexString().length)
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database')
@@ -16,27 +12,27 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    // db.collection('users').findOne({ _id: new ObjectId("6204eeb4b09e916fe28c2d5b") }, (error, user) => {
-    //     if (error) {
-    //         return console.log('Unable to fetch')
+    // db.collection('users').updateOne({ 
+    //     _id: new ObjectId("6204e01a9709bccb1f29e4a3")
+    // }, {
+    //     $inc: {
+    //         age: 1
     //     }
-
-    //     console.log(user)
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch(() => {
+    //     console.log(error)
     // })
 
-    // db.collection('users').find({ age: 25 }).toArray((error, users) =>{
-    //     console.log(users)
-    // })
-
-    // db.collection('users').find({ age: 25 }).count((error, users) =>{
-    //     console.log(users)
-    // })
-
-    db.collection('tasks').findOne({ _id: new ObjectId("6204eeb4b09e916fe28c2d5e") }, (error, task) => {
-        console.log(task)
-    })
-
-    db.collection('tasks').find({ completed: false }).toArray((error, task) => {
-        console.log(task)
+    db.collection('tasks').updateMany({
+        completed: false
+    }, {
+        $set: {
+            completed: true
+        }
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
     })
 })
