8beb0b3b89166ef09d4d1d9c7d2e880580fe410b
bendecre <bed1@umbc.edu>
2022-02-14 14:31:53 -0500

    Data sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,43 +1,43 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
-// const me = new User({
-//     name: 'Ben',
-//     age: 25
-// })
-
-// me.save().then((me) => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error: ', error)
-// })
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
+const me = new User({
+    name: '   Ben    ',
+    email: 'MYEMAIL@gmail.com'
 })
 
-const task = new Task({
-    description: 'Buy milk',
-    completed: false
-})
-
-task.save().then((task) =>{
-    console.log(task)
+me.save().then((me) => {
+    console.log(me)
 }).catch((error) => {
-    console.log(error)
+    console.log('Error: ', error)
 })
