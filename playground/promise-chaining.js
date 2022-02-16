c73dd65503314e1148e36fafeb34899c8665cac7
bendecre <bed1@umbc.edu>
2022-02-16 13:53:05 -0500

    more promise chaining code

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('620ac434ef049c30af49e79a', { age: 1 }).then((user) =>{
-    console.log(user)
-    return User.countDocuments({ age: 1 })
-}).then((result) => {
-    console.log(result)
+// User.findByIdAndUpdate('620ac434ef049c30af49e79a', { age: 1 }).then((user) =>{
+//     console.log(user)
+//     return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('620ac434ef049c30af49e79a', 2).then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
