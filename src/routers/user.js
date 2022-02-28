efd2b1802f4807b5a6ab6e42c957ca68383a993c
bendecre <bed1@umbc.edu>
2022-02-28 14:32:37 -0500

    added authentication for user routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -53,23 +53,18 @@ router.get('/users/me', auth, async (req, res) =>{
     res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     const _id = req.params.id
     
     try{
-        const user = await User.findById(_id)
-
-        if(!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch (e) {
         res.status(500).send()
     }
 })
 
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -79,19 +74,13 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try{
-        const user = await User.findById(req.params.id)
-
         updates.forEach((update) => {
-            user[update] = req.body[update]
+            req.user[update] = req.body[update]
         })
 
-        await user.save()
+        await req.user.save()
     
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        res.send(req.user)
     } catch(e) {
         
     }
