80719326eb72c18c69c4b2fa5d400522925ef90a
bendecre <bed1@umbc.edu>
2022-03-22 04:05:03 -0400

    added support for word document uploading

--- src/index.js
+++ src/index.js
@@ -6,13 +6,23 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// const multer = require('multer')
-// const upload = multer({
-//     dest: 'images'
-// })
-// app.post('/upload', upload.single('upload'), (req, res) => {
-//     res.send()
-// })
+const multer = require('multer')
+const upload = multer({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc | docx)$/)) {
+            return cb(new Error('Please upload a word document'))
+        }
+
+        cb(undefined, true)
+    }
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
 
 app.use(express.json())
 app.use(userRouter)
