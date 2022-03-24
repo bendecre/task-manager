808e1598104995673cb838b6bce4dc4bdce0dc97
bendecre <bed1@umbc.edu>
2022-03-24 15:19:14 -0400

    staging for final commit

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,6 +4,7 @@ const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const { application } = require('express')
+const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -11,6 +12,7 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
@@ -59,6 +61,7 @@ router.get('/users/me', auth, async (req, res) =>{
 router.delete('/users/me', auth, async (req, res) => {
     try{
         await req.user.remove()
+        sendCancelationEmail(req.user.email, req.user.name)
         res.send(req.user)
     } catch (e) {
         res.status(500).send()
